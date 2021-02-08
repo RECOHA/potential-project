db3f5eede1e9c928dddfeaed293653616d549a5a
Renan Hagiwara <renan@recoha.com>
2021-02-08 04:33:05 -0300

    Criação do arquivo de instalação do projeto.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Iniciando instalação..."
+echo ""
+
+# Subindo o backend.
+cd backend
+docker-compose up -d
+
+docker exec -t app-php sh -c "
+    cd html &&
+    composer install &&
+    cp .env.example .env &&
+    php artisan key:generate &&
+    php artisan migrate:fresh --seed
+"
+
+echo "Backend executando em: http://localhost:8000/"
+read -p "Pressione um botão para continuar..."
+
+# Subindo o frontend.
+cd ..
+cd frontend
+npm run dev
+
+echo "Frontend executando em: http://localhost:8080/"
+read -p "Pressione um botão para encerrar..."
